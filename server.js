aea752ebb1e3553cc7d0abc1c51c6e1d44067cd7
Francisco-Mtz <isa.franciscomtz@hotmail.com>
2023-08-02 16:19:01 -0600

    Se mejora configuración del server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/angular-app-heroku'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/portafolio-personal/'}),
+    res.sendFile('index.html', {root: 'dist/portafolio-personal/src/'}),
 );
 
 // Start the app by listening on the default Heroku port
